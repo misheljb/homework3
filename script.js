d55a547517ed7699b356e349c7762275b1eeb4ae
Mishel Balderrama <mishelbalderrama@gmail.com>
2020-11-24 14:29:44 -0500

    added arrays, and prompts

--- script.js
+++ script.js
@@ -11,8 +11,37 @@ function writePassword() {
 
 }
 
+var numbers = [0,1,2,3,4,5,6,7,8,9,]
+var specialcharacters = ["!","@","#","$","%","^","&","*","~","?","<",">"]
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+var passwordoptions =[]
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", function (event) {
   event.preventDefault()
-  alert("Generating Password")
+  alert("Let's get started")
+
+  var passlength = prompt("How long would you like your password to be?")
+      if (passlength > 128 ) { 
+        alert ("Please choose number between 8 and 128 characters")
+      } else {
+        var addNumbers = confirm("Would you like to add numbers?")
+      if (addNumbers) {
+        passwordoptions.concat(numbers)
+      }
+    
+    
+    
+    }
+        
+
+      
+
+
+
+
 })
+
